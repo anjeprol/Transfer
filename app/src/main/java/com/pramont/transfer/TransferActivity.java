16d8a7a99e498725a24abe4e7e63525da21c1f71
antoniopradoo <antoniopradoo@gmail.com>
2016-10-09 19:10:12 -0500

    agregando las etapas

--- app/src/main/java/com/pramont/transfer/TransferActivity.java
+++ app/src/main/java/com/pramont/transfer/TransferActivity.java
@@ -1,13 +1,91 @@
 package com.pramont.transfer;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
 
-public class TransferActivity extends AppCompatActivity {
+public class TransferActivity extends AppCompatActivity implements View.OnClickListener {
+    private String mCtaPaypay;
+    private TextView mCtaPaypalTextView;
+    private Button mCtaButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transfer);
+
+        mCtaPaypalTextView = (TextView) findViewById(R.id.cta_paypal);
+        mCtaButton = (Button) findViewById(R.id.ctaPayPal);
+
+        mCtaButton.setOnClickListener(this);
+    }
+
+    private void alertCta(){
+        // get alert_cta.xml view
+        LayoutInflater layoutInflater = LayoutInflater.from(this);
+        View promptsView = layoutInflater.inflate(R.layout.alert_cta, null);
+
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+
+        // set prompts.xml to alertdialog builder
+        alertDialogBuilder.setView(promptsView);
+
+        final EditText userInput = (EditText) promptsView.findViewById(R.id.InputAlertDialog);
+        userInput.requestFocus();
+
+        ((InputMethodManager)this.getSystemService(Context.INPUT_METHOD_SERVICE)).
+                toggleSoftInput(InputMethodManager.SHOW_FORCED, InputMethodManager.HIDE_IMPLICIT_ONLY);
+
+        // set dialog message
+        alertDialogBuilder
+                .setCancelable(false)
+                .setPositiveButton("OK",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,int id) {
+                                String newAmount;
+                                // get user input and set it to result
+                                // edit text
+                                newAmount = userInput.getText().toString().trim();
+                                if(!newAmount.isEmpty() && !newAmount.equalsIgnoreCase("0") )
+                                {
+                                    mCtaPaypay = userInput.getText().toString().trim();
+                                    mCtaPaypalTextView.setText(mCtaPaypay);
+                                }
+                               // donate();
+                               // hideKeyboard();
+                            }
+                        })
+                .setNegativeButton("Cancelar",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,int id) {
+                                dialog.cancel();
+                               // hideKeyboard();
+                            }
+                        });
+
+        // create alert dialog
+        AlertDialog alertDialog = alertDialogBuilder.create();
+
+        // show it
+        alertDialog.show();
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.ctaPayPal:
+                alertCta();
+                break;
+        }
     }
 }
